1b9eb7e2d2fbb480ed328aa4c4b0ef0e20c45dd6
Andrew Allbright <aallbrig@gmail.com>
2022-03-18 14:35:09 -0400

    Work around incorrect puppeteer Dockerfile (w/ TODO)

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-docker build -f ./src/Dockerfile -t html-static-webserver -l./src
-docker build -f ./acceptance-tests/Dockerfile -t acceptance-tests ./acceptance-tests
+docker build -f ./src/Dockerfile -t html-static-webserver ./src
+# TODO: Get puppeteer script working inside k8s container
+# docker build -f ./acceptance-tests/Dockerfile -t acceptance-tests ./acceptance-tests
 
 docker run -l app=static -d -p 80:8000 html-static-webserver
 
-docker run acceptance-tests
+# TODO: Get puppeteer script working inside k8s container
+# docker run acceptance-tests
+HEADLESS_MODE=false LANDING_PAGE="http://localhost:8000" npm --prefix ./acceptance-tests test
 
 docker stop "$(docker ps --filter label=app=static --format "{{ .ID }}")"
