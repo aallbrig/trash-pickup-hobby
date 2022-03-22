214b24b089da4ca5a828063cbd57df08a613cb24
Andrew Allbright <aallbrig@gmail.com>
2022-03-21 20:29:28 -0400

    Touch up the site

--- scripts/website-up.sh
+++ scripts/website-up.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -f ./src/Dockerfile -t html-static-webserver ./src
-docker run -l app=static -d -p 80:8000 html-static-webserver
+# docker build -f ./src/Dockerfile -t html-static-webserver ./src
+# docker run -l app=static -d -p 80:8000 html-static-webserver
+docker run -l app=static -d -p 8888:80 -v $(pwd)/src:/usr/share/nginx/html nginx:alpine
