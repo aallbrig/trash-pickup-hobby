9307468bd53c5256cb442fda79a2ec5e56712afe
aallbrig <aallbrig@users.noreply.github.com>
2022-03-25 17:33:49 +0000

    Deploying to github-pages from @ aallbrig/trash-pickup-hobby@cfb85303c3a4ffa955ea40ead38ffa0148414d19 🚀

---
+++ docs/WebGL/TemplateData/main.js
@@ -0,0 +1,14 @@
+var canvas = document.querySelector("#unity-canvas");
+
+var config = {
+    dataUrl: "Build/WebGL.data",
+    frameworkUrl: "Build/WebGL.framework.js",
+    codeUrl: "Build/WebGL.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "Andrew Allbright",
+    productName: "Trash Pickup Video Game",
+    productVersion: "0.0.34",
+    devicePixelRatio: 1,
+}
+
+createUnityInstance(canvas, config);
