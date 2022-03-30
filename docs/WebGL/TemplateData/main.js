4f481de4572e2fff613eac71456fb8f06ee4eb63
aallbrig <aallbrig@gmail.com>
2022-03-30 14:09:26 +0000

    Deploying to github-pages from @ aallbrig/trash-pickup-hobby@e7d5778b4eb327d6dfdb9a8fedf7989c5f8d5ef8 🚀

--- docs/WebGL/TemplateData/main.js
+++ docs/WebGL/TemplateData/main.js
@@ -7,7 +7,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Andrew Allbright",
     productName: "Trash Pickup Video Game",
-    productVersion: "0.0.59",
+    productVersion: "0.0.60",
     devicePixelRatio: 1,
 }
 
